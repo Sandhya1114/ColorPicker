820c0103c8f3c52e958ba5eb8cffb8c7b6d52649
Sandhya <sandhyarana07610761@gmail.com>
2025-06-22 00:21:42 +0530

    commit

--- src/components/History/HistoryPage.jsx
+++ src/components/History/HistoryPage.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 import './HistoryPage.css';
 import Footer from '../Footer/Footer';
 
@@ -24,27 +24,30 @@ export default function HistoryPage({ history }) {
   return (
     <>
        <div className="historyPageContainer">
-      <button className="backBtn" onClick={() => navigate(-1)}>← Back</button>
-      <h2>Image History</h2>
-      {history.length === 0 ? (
-        <p>No history yet.</p>
-      ) : (
-        <div className="historyList">
-          {history.map((item, idx) => (
-            <div key={idx} className="historyItem" onClick={() => handleSelect(item)}>
-              <img src={item.src} alt={`History-${idx}`} className="historyThumb" />
-              <div className="colorGrid">
-                {item.palette.map((c, i) => (
-                  <div key={i} className="colorSwatch">
-                    <div className="colorBox" style={{ backgroundColor: rgbToHex(...c) }} />
-                    <span className="hexCode">{rgbToHex(...c)}</span>
+          <button className="backBtn" onClick={() => navigate(-1)}>← Back</button>
+           <button className="backBtn" ><Link to="/upload"> Palette Generator</Link></button>
+          
+          <h2 className='HistoryHeading'>Searched Images</h2>
+          <p className='historyLine'>click on any image to get more clearly see and customize</p>
+          {history.length === 0 ? (
+            <p>No history yet.</p>
+          ) : (
+            <div className="historyList">
+              {history.map((item, idx) => (
+                <div key={idx} className="historyItem" onClick={() => handleSelect(item)}>
+                  <img src={item.src} alt={`History-${idx}`} className="historyThumb" />
+                  <div className="colorGrid">
+                    {item.palette.map((c, i) => (
+                      <div key={i} className="colorSwatch">
+                        <div className="colorBox" style={{ backgroundColor: rgbToHex(...c) }} />
+                        <span className="hexCode">{rgbToHex(...c)}</span>
+                      </div>
+                    ))}
                   </div>
-                ))}
-              </div>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      )}
+          )}
        </div>
        <Footer/>
     </>
